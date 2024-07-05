429bdf24444695e8a97839b7b645d004b2fba66a
chaoYouZhi <31413924+chaoYouZhi@users.noreply.github.com>
2024-07-05 22:29:37 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,10 +26,10 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'LoongX', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '提供优质的集运服务', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://loongx.ing', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '集运服务, 英国留学生', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '集运服务, 国际物流', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'loongxing01@gmail.com', // 邮箱地址 例如mail@tangly1024.com
+  CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 例如 https://github.com/tangly1024
