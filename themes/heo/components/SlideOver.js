c2665e4dbd311ff98b2f536a3340514a1db72693
chaoYouZhi <31413924+chaoYouZhi@users.noreply.github.com>
2024-07-05 20:52:09 +0800

    Update SlideOver.js

--- themes/heo/components/SlideOver.js
+++ themes/heo/components/SlideOver.js
@@ -84,7 +84,7 @@ export default function SlideOver(props) {
                                             </section>
 
                                             <section className='space-y-2 flex flex-col'>
-                                                <div>博客</div>
+                                                <div>导航</div>
                                                 {/* 导航按钮 */}
                                                 <div className='gap-2 grid grid-cols-2'>
                                                     <Button title={'主页'} url={'/'} />
